7b5b91442c6530de39abc57477bb358cfe2481e6
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-10-17 22:08:29 -0700

    database setup for new laptop.

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker', 'root', '5454', {
+const sequelize = new Sequelize('expense-tracker', 'root', 'Hawking@12', {
     dialect : 'mysql',
     host : 'localhost'
 })
